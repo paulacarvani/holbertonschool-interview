00829abe8919f76fc5bec83a8bb4c9b1bb76084d
paulacarvani <3312@holbertonschool.com>
2022-10-06 20:04:15 -0500

    All files

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""0. UTF-8 Validation"""
+
+
+def validUTF8(data):
+    """Determines if a given data set represents a valid UTF8 encoding"""
+    count = 0
+
+    for x in data:
+
+        if 191 >= x >= 128:
+
+            if not count:
+                return False
+
+            count -= 1
+        else:
+            if count:
+                return False
+
+            if x < 128:
+                continue
+            elif x < 224:
+                count = 1
+            elif x < 240:
+                count = 2
+            elif x < 248:
+                count = 3
+            else:
+                return False
+
+    return count == 0
