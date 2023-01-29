0c0cd83205d860e52186992685c92b5022bb8a08
paulacarvani <3312@holbertonschool.com>
2023-01-29 17:08:09 -0500

    0x11-heap_sort task

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -27,14 +27,14 @@ void heapify(int *arr, int n, int i, size_t size)
 	int right_child = 2 * i + 2;
 
 	if (left_child < n && arr[left_child] > arr[biggest])
-    {
+{
 		biggest = left_child;
-    }
+}
 
 	if (right_child < n && arr[right_child] > arr[biggest])
-    {
+{
 		biggest = right_child;
-    }
+}
 
 	if (biggest != i)
 	{
@@ -54,14 +54,14 @@ void heap_sort(int *array, size_t size)
 	size_t i;
 
 	if (!array)
-    {
+{
 		return;
-    }
+}
 
 	for (i = size / 2; i > 0; i--)
-    {
+{
 		heapify(array, size, i - 1, size);
-    }
+}
 
 	for (i = size - 1; i > 0; i--)
 	{
