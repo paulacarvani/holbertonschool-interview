bec1b7da84a817424f23fe70973e1e1446c6f965
paulacarvani <3312@holbertonschool.com>
2023-04-09 19:22:33 -0500

    Checking solutions

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,43 +1,38 @@
 #!/usr/bin/python3
-'''
-Module that solves the N Queens puzzle
-'''
-from sys import argv, exit
+"""
+Program that solves the N queens problem
+"""
+
+
+import sys
+
 
 if __name__ == "__main__":
-    if len(argv) != 2:
-        print('Usage: nqueens N')
-        exit(1)
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
     try:
-        n = int(argv[1])
-    except BaseException:
-        print('N must be a number')
-        exit(1)
-    if n < 4:
-        print('N must be at least 4')
-        exit(1)
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
 
-    solution = []
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
 
-    def solve_queens(row, n, solution):
-        if (row == n):
-            print(solution)
-        else:
-            for col in range(n):
-                placement = [row, col]
-                if valid_placement(solution, placement):
-                    solution.append(placement)
-                    solve_queens(row + 1, n, solution)
-                    solution.remove(placement)
 
-    def valid_placement(solution, placement):
-        for queen in solution:
-            if queen[1] == placement[1]:
-                return False
-            if (queen[0] + queen[1]) == (placement[0] + placement[1]):
-                return False
-            if (queen[0] - queen[1]) == (placement[0] - placement[1]):
-                return False
-        return True
+def backtracking(N, i=0, a=[], b=[], c=[]):
+    """Generates backtracking solutions for N queens problem"""
+    if i < N:
+        for j in range(N):
+            if j not in a and i+j not in b and i-j not in c:
+                for solution in backtracking(N, i+1, a+[j], b+[i+j], c+[i-j]):
+                    yield solution
+    else:
+        yield a
 
-    solve_queens(0, n, solution)
+for solution in backtracking(N):
+    answer = [[col, row] for col, row in enumerate(solution)]
+    print(answer)
